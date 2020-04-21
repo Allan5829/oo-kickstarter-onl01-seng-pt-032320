b1a5051d5ccb84e3c486a7b0fd92c22525eab3c9
Allan Farca <allan.farca@gmail.com>
2020-04-21 06:25:39 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,13 @@
 class Project
-  attr_accessor :title, :backers
-
-  def initialize(title)
+  attr_accessor :backers, :title
+  
+  def initialize (title)
     @title = title
     @backers = []
-  end
-
-  def add_backer(backer)
+  end 
+  
+  def add_backer (backer)
     @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
-  end
-end
+  end 
+  
+end 
